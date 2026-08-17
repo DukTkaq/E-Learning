eb5961afb456b37c318f86e0198d5c153908abcf
dangkhoa <khoatdce192037@gmail.com>
2026-08-17 20:32:28 +0700

    feat(category): implement UC-24.2 update category with validation

--- server/src/routes/categoryRoutes.js
+++ server/src/routes/categoryRoutes.js
@@ -6,5 +6,6 @@ const { authorizeRoles } = require('../middlewares/roleMiddleware');
 
 router.post('/', verifyToken, authorizeRoles('admin'), categoryController.createCategory);
 router.get('/', verifyToken, authorizeRoles('admin'), categoryController.getAllCategories);
+router.put('/:id', verifyToken, authorizeRoles('admin'), categoryController.updateCategory);
 
 module.exports = router;
